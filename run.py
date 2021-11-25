c9af34525903f2b701990ab6f9d1f5fb44a80a9e
Alisha Sehgal <alishaseh@gmail.com>
2021-11-25 01:39:43 -0800

    Update run.py

---
+++ run.py
@@ -0,0 +1,11 @@
+import json
+from src import tweet_hydrate
+from src import tweet_download
+from src import percentage_eda
+import test
+from test import data_creation
+
+num_tweets = 500
+
+
+test.test_tweets(num_tweets)
